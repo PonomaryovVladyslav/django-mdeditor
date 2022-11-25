262a7995fc22ac8312e843ce789b8880d6f86139
VladyslavPonomaryov <vladyslav.ponomaryov@ab-inbev.com>
2022-11-25 14:17:21 +0100

    test upd

--- mdeditor/views.py
+++ mdeditor/views.py
@@ -22,61 +22,27 @@ class UploadView(generic.View):
 
     def post(self, request, *args, **kwargs):
         upload_image = request.FILES.get("editormd-image-file", None)
-        media_root = settings.MEDIA_ROOT
-
         # image none check
         if not upload_image:
             return JsonResponse({
                 'success': 0,
-                'message': "未获取到要上传的图片",
+                'message': "Doesn't contain picture",
                 'url': ""
             })
-
-        # image format check
-        file_name_list = upload_image.name.split('.')
-        file_extension = file_name_list.pop(-1)
-        file_name = '.'.join(file_name_list)
-        if file_extension not in MDEDITOR_CONFIGS['upload_image_formats']:
-            return JsonResponse({
-                'success': 0,
-                'message': "上传图片格式错误，允许上传图片格式为：%s" % ','.join(
-                    MDEDITOR_CONFIGS['upload_image_formats']),
-                'url': ""
-            })
-
-        # image floder check
-        file_path = os.path.join(media_root, MDEDITOR_CONFIGS['image_folder'])
-        file_path = file_path.replace(os.sep, '/')
-        if not os.path.exists(file_path):
-            try:
-                os.makedirs(file_path)
-            except Exception as err:
-                return JsonResponse({
-                    'success': 0,
-                    'message': "上传失败：%s" % str(err),
-                    'url': ""
-                })
-
-        # save image
-        file_full_name = '%s_%s.%s' % (file_name,
-                                       '{0:%Y%m%d%H%M%S%f}'.format(datetime.datetime.now()),
-                                       file_extension)
         try:
             from app.models import Picture
             new_picture = Picture(img=upload_image)
             new_picture.save()
-            print('aaaaaaaaaaaaaaa')
+            url = new_picture.img.url
+            if not url.startswith('http'):
+                url = 'http://127.0.0.1:8000' + url
+            return JsonResponse({'success': 1,
+                                 'message': "All good",
+                                 'url': url})
         except ImportError:
-            print('Import error')
-        # with open(os.path.join(file_path, file_full_name), 'wb+') as file:
-        #     for chunk in upload_image.chunks():
-        #         file.write(chunk)
-        url = os.path.join(settings.MEDIA_URL,
-                           MDEDITOR_CONFIGS['image_folder'],
-                           file_full_name)
-        url = url.replace(os.sep, '/')
-        if not url.startswith('http'):
-            url = 'http://127.0.0.1:8000' + url
-        return JsonResponse({'success': 1,
-                             'message': "上传成功！",
-                             'url': url})
+            return JsonResponse({
+                    'success': 0,
+                    'message': "Cannot get app.Picture",
+                    'url': ""
+                })
+
